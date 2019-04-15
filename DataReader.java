e3e073ad8bdcb78cb0b36934e58d9e0dfb8145c9
anvitha2828 <anvitha2828@gmail.com>
2019-04-15 16:25:48 -0400

    Add files via upload

--- DataReader.java
+++ DataReader.java
@@ -1,8 +1,12 @@
 package prj5;
 
 public class DataReader {
-    
+    GUIDataWindow dw;
+
+
     public DataReader(String songFile, String personFile) {
+        dw = new GUIDataWindow();
+
         // throw FileNotFoundException,
     }
 }
