2e338d683a9c0701db748977cc7db5414b98f6ef
mpinho16 <49571066+mpinho16@users.noreply.github.com>
2019-04-16 23:41:56 -0400

    Update LListTest.java

--- prj5/LListTest.java
+++ prj5/LListTest.java
@@ -1,12 +1,23 @@
 package prj5;
 
+/**
+ * Test class for LList.
+ * 
+ * @author Matthew Pinho
+ * @author Peter Kistler
+ * @author Nicholas Cardaci
+ * @author Anvitha Nachiappan
+ * @version 2019.04.16
+ */
 public class LListTest extends student.TestCase {
     private LList list;
     private Song jack;
     private Song queen;
     private Song king;
 
-
+    /**
+     * Sets up before each test method run.
+     */
     public void setUp() {
         list = new LList();
         jack = new Song("Jack","Hearts", "1800", "Metal");
@@ -17,15 +28,60 @@ public class LListTest extends student.TestCase {
         list.add(queen);
         list.add(king);
     }
+    
+    /**
+     * Checks that songs are added correctly.
+     */
+    public void testAdd() {
+        assertEquals(list.size(), 3);
+        assertFalse(list.isEmpty());
+        
+        Exception thrown = null;
+        try {
+            list.add(null);
+        }
+        catch (Exception e) {
+            thrown = e;
+        }
+        assertNotNull(thrown);
+        
+        list.clear();
+        list.clear();
+        assertTrue(list.isEmpty());
+        Exception thrown1 = null;
+        try {
+            list.get(-1);
+        }
+        catch (Exception e) {
+            thrown1 = e;
+        }
+        assertNotNull(thrown1);
+        Exception thrown2 = null;
+        try {
+            list.get(1);
+        }
+        catch (Exception e) {
+            thrown2 = e;
+        }
+        assertNotNull(thrown2);
+        list.add(jack);
+        assertEquals(jack, list.get(0));
+        list.add(queen);
+        assertEquals(queen, list.get(1));
+    }
 
-
+    /**
+     * Checks that swaps two elements in the list.
+     */
     public void testSwapElements() {
         list.swapElements(0, 1);
         assertEquals(queen, list.get(0));
         assertEquals(jack, list.get(1));
     }
 
-
+    /**
+     * Checks that second input is earlier than first.
+     */
     public void testHasFoundEarliest() {
         String str1 = "ab";
         String str2 = "bc";
@@ -34,7 +90,9 @@ public class LListTest extends student.TestCase {
         assertTrue(list.foundNewEarliest(str2, str1));
     }
 
-
+    /**
+     * Checks that sortation is done correctly.
+     */
     public void testSortBy() {
         list.sortBy("title");
         assertEquals(jack, list.get(0));
