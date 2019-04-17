9f237ecf31c089c5ff84a378c0535e4bcc388a9f
anvitha2828 <anvitha2828@gmail.com>
2019-04-16 23:36:59 -0400

    Add files via upload

--- prj5/HobbyEnum.java
+++ prj5/HobbyEnum.java
@@ -1,9 +1,17 @@
 package prj5;
 
+/**
+ * StateEnum holds the values READ, ART, SPORTS, MUSIC
+ * 
+ * @author Anvitha Nachiappan <anvitha>
+ * @author Nicholas Cardaci <nicho17>
+ * @author Matthew Pinho <mpinho16>
+ * @author Peter Kistler <pdblvkis>
+ *
+ */
 public enum HobbyEnum {
     READ, ART, SPORTS, MUSIC;
-    HobbyEnum()
-    {
-        
+    HobbyEnum() {
+
     }
 }
--- prj5/MajorEnum.java
+++ prj5/MajorEnum.java
@@ -1,9 +1,17 @@
 package prj5;
 
+/**
+ * StateEnum holds the values COMP_SCI, OTHER_ENGR, MATH_CMDA, OTHER
+ * 
+ * @author Anvitha Nachiappan <anvitha>
+ * @author Nicholas Cardaci <nicho17>
+ * @author Matthew Pinho <mpinho16>
+ * @author Peter Kistler <pdblvkis>
+ *
+ */
 public enum MajorEnum {
     COMP_SCI, OTHER_ENGR, MATH_CMDA, OTHER;
-    MajorEnum()
-    {
-        
+    MajorEnum() {
+
     }
 }
