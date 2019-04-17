32c79686765808c6f30a5dc26a98bc946ce52a7e
mpinho16 <49571066+mpinho16@users.noreply.github.com>
2019-04-16 21:27:24 -0400

    Update LList.java

--- prj5/LList.java
+++ prj5/LList.java
@@ -1,318 +1,351 @@
 package prj5;
 
-import java.util.Arrays;
-import java.util.Iterator;
-import prj5.Song;
+import java.awt.Color;
+import CS2114.Button;
+import CS2114.Shape;
+import CS2114.TextShape;
+import CS2114.Window;
+import CS2114.WindowSide;
 
-public class LList implements Iterable<Song> {
+/**
+ * 
+ * @author Matthew Pinho
+ *
+ */
+public class GUIDataWindow {
+    // private Sorter s;
+    private Window window;
+    private Button quit;
+    private Button next;
+    private Button previous;
+    private Button representByHobby;
+    private Button representByMajor;
+    private Button representByState;
+    private Button sortByArtistName;
+    private Button sortBySongTitle;
+    private Button sortByReleaseYear;
+    private Button sortByGenre;
+    private int pageNumber; // starts at 0.
+    private String represent;
+    private final int columnWidth = 10;
+    private final int columnHeight = 40;
+    private final int lastPage;
+    private final int barLengthFactor = 10;
+    private final int barWidth = 10;
+    private LList list;
+
 
     /**
      * 
-     * @version 4/14/2015
-     * @version 9.4.15
-     * @version 10.29.15
-     * @version 10/15/2016
-     * @version 03/17/2017
+     * @param songs
      */
-    public static class Node {
+    public GUIDataWindow(LList songs) {
+        represent = "hobby";
+        list = songs;
+        lastPage = (int)Math.ceil(list.size() / 9);
 
-        // The data element stored in the node.
-        private Song data;
+        // create window
+        window = new Window("Song Survey Visualization");
 
-        // The next node in the sequence.
-        private Node next;
+        // create buttons
+        quit = new Button("Quit");
+        previous = new Button("<-- Prev");
+        next = new Button("Next -->");
+        representByHobby = new Button("Represent Hobby");
+        representByMajor = new Button("Represent Major");
+        representByState = new Button("Represent State");
+        sortByArtistName = new Button("Sort by Artist Name");
+        sortBySongTitle = new Button("Sort by Song Title");
+        sortByReleaseYear = new Button("Sort by Release Year");
+        sortByGenre = new Button("Sort by Genre");
 
+        // add to window
+        window.addButton(quit, WindowSide.SOUTH);
+        window.addButton(previous, WindowSide.NORTH);
+        window.addButton(representByState, WindowSide.SOUTH);
+        window.addButton(representByMajor, WindowSide.SOUTH);
+        window.addButton(representByHobby, WindowSide.SOUTH);
+        window.addButton(sortByArtistName, WindowSide.NORTH);
+        window.addButton(sortByGenre, WindowSide.NORTH);
+        window.addButton(sortByReleaseYear, WindowSide.NORTH);
+        window.addButton(sortBySongTitle, WindowSide.NORTH);
+        window.addButton(next, WindowSide.NORTH);
 
-        /**
-         * Creates a new node with the given data
-         *
-         * @param d
-         *            the data to put inside the node
-         */
-        public Node(Song d) {
-            data = d;
+        sortBySongTitle.onClick(this, "clickedSortBySongTitle");
+        sortByReleaseYear.onClick(this, "clickedSortBySongTitle");
+        sortByGenre.onClick(this, "clickedSortBySongTitle");
+        sortByArtistName.onClick(this, "clickedSortBySongTitle");
+        quit.onClick(this, "clickedQuit");
+        previous.onClick(this, "clickedPrevious");
+        next.onClick(this, "clickedNext");
+        representByState.onClick(this, "clickedRepresentByState");
+        representByMajor.onClick(this, "clickedRepresentByMajor");
+        representByHobby.onClick(this, "clickedRepresentByHobby");
+
+        update();
+        previous.disable();
+        if (lastPage - 1 <= 0) {
+            next.disable();
         }
-
-
-        /**
-         * Sets the node after this node
-         *
-         * @param n
-         *            the node after this one
-         */
-        public void setNext(Node n) {
-            next = n;
-        }
-
-
-        /**
-         * Gets the next node
-         *
-         * @return the next node
-         */
-        public Node next() {
-            return next;
-        }
-
-
-        /**
-         * Gets the data in the node
-         *
-         * @return the data in the node
-         */
-        public Song getData() {
-            return data;
-        }
-
-
-        public void setData(Song data2) {
-
-        }
-    }
-
-    private Node head;
-    private int size;
-    private String[] param;
-    private LListIterator iter;
-
-
-    /**
-     * Creates a new LinkedList object
-     */
-    public LList() {
-        head = null;
-        size = 0;
-        param = new String[0];
     }
 
 
     /**
-     * Gets the number of elements in the list
-     *
-     * @return the number of elements
-     */
-    public int size() {
-        return size;
-    }
-
-
-    /**
-     * Adds the object to the end of the list.
-     *
-     * @precondition song cannot be null
-     * @param song
-     *            the object to add
-     * @throws IllegalArgumentException
-     *             if song is null
-     */
-    public void add(Song song) {
-        iter = iterator();
-
-        if (song == null) {
-            throw new IllegalArgumentException("Cannot add null "
-                + "objects to a list");
-        }
-
-        if (isEmpty()) {
-            head = new Node(song);
-        }
-        else {
-            Node curr = head;
-            while (iter.hasNext()) {
-                iter.next();
-                curr = curr.next;
-            }
-
-            curr.next = new Node(song);
-        }
-
-        size++;
-        param = new String[size];
-    }
-
-
-    /**
-     * Checks if the array is empty
-     *
-     * @return true if the array is empty
-     */
-    public boolean isEmpty() {
-        return size == 0;
-    }
-
-
-    /**
-     * Gets the object at the given position
-     *
-     * @param index
-     *            where the object is located
-     * @return The object at the given position
-     * @throws IndexOutOfBoundsException
-     *             if no node at the given index
-     */
-    public Song get(int index) {
-        iter = iterator();
-
-        if (index < 0 || size() <= index) {
-            throw new IndexOutOfBoundsException("No element exists at "
-                + index);
-        }
-
-        Song song = head.data;
-
-        for (int i = 0; i < index; i++) {
-            song = iter.next();
-        }
-
-        return song;
-    }
-
-
-    private void changeParam(String str) {
-        if (str == "title") {
-            for (int i = 0; i < size; i++) {
-                param[i] = get(i).getTitle();
-            }
-        }
-        else if (str == "genre") {
-            for (int i = 0; i < size; i++) {
-                param[i] = get(i).getGenre();
-            }
-        }
-        else if (str == "artist") {
-            for (int i = 0; i < size; i++) {
-                param[i] = get(i).getArtist();
-            }
-        }
-        else if (str == "year") {
-            for (int i = 0; i < size; i++) {
-                param[i] = get(i).getYear();
-            }
-        }
-        else {
-            throw new IllegalArgumentException();
-        }
-
-    }
-
-
-    /**
-     * Sorts the list by input parameter.
      * 
-     * @param str
-     *            Song parameter by which is sorted.
      */
-    public void sortBy(String str) {
-        changeParam(str); // param is now set according to input parameter.
+    public void clickedSortBySongTitle() {
+        list.sortBy("title");
+        update();
+    }
 
-        for (int lh = 0; lh < size - 1; lh++) {
-            String earliest = param[lh];
-            int pEarliest = -1; // default values.
-            String curr;
 
-            for (int rh = lh + 1; rh < size; rh++) {
-                curr = param[rh]; // String on rh to compare.
+    /**
+     * 
+     */
+    public void clickedSortByArtistName() {
+        list.sortBy("artist");
+        update();
+    }
 
-                // If earlier than earliest, new earliest.
-                if (foundNewEarliest(earliest, curr)) {
-                    earliest = curr;
-                    pEarliest = rh;
+
+    /**
+     * 
+     */
+    public void clickedSortByGenre() {
+        list.sortBy("genre");
+        update();
+    }
+
+
+    /**
+     * 
+     */
+    public void clickedSortByReleaseYear() {
+        list.sortBy("year");
+        update();
+    }
+
+
+    /**
+     * 
+     */
+    public void clickedRepresentByMajor() {
+        represent = "major";
+        update();
+    }
+
+
+    /**
+     * 
+     */
+    public void clickedRepresentByState() {
+        represent = "state";
+        update();
+    }
+
+
+    /**
+     * 
+     */
+    public void clickedRepresentByHobby() {
+        represent = "hobby";
+        update();
+    }
+
+
+    /**
+     * Closes the GUI.
+     */
+    public void clickedQuit() {
+        System.exit(0);
+    }
+
+
+    /**
+     * Simulates clicking "Next" on the GUI.
+     */
+    public void clickedNext() {
+        pageNumber++;
+        update();
+        if (pageNumber >= lastPage) {
+            next.disable();
+        }
+        else {
+            next.enable();
+        }
+    }
+
+
+    /**
+     * Simulates clicking "Previous" on the GUI.
+     */
+    public void clickedPrevious() {
+        pageNumber--;
+        update();
+        if (pageNumber <= 0) {
+            previous.disable();
+        }
+        else {
+            previous.enable();
+        }
+    }
+
+
+    /**
+     * Updates everything in GUI as needed in response to clicks on the Window.
+     */
+    private void update() {
+        updateLegend();
+        updateGlyphs();
+    }
+
+
+    /**
+     * Updates legend in response to change in representation.
+     */
+    private void updateLegend() {
+        // background
+        window.addShape(new Shape(10, 10, 10, 10));
+
+        // bar
+        window.addShape(new Shape(10, 10, 10, 10));
+        window.addShape(new TextShape(10, 10, "Song"));
+        window.addShape(new TextShape(10, 10, "Heard"));
+        window.addShape(new TextShape(10, 10, "Likes"));
+
+        TextShape title;
+        TextShape enumVal1;
+        TextShape enumVal2;
+        TextShape enumVal3;
+        TextShape enumVal4;
+
+        if (represent == "major") {
+            title = new TextShape(10, 10, "Major Legend");
+            enumVal1 = new TextShape(10, 10, "Comp Sci");
+            enumVal2 = new TextShape(10, 10, "Math & CMDA");
+            enumVal3 = new TextShape(10, 10, "Other Engineering");
+            enumVal4 = new TextShape(10, 10, "Other");
+        }
+        else if (represent == "state") {
+            title = new TextShape(10, 10, "State Legend");
+            enumVal1 = new TextShape(10, 10, "Northeast US");
+            enumVal2 = new TextShape(10, 10, "SouthEast US");
+            enumVal3 = new TextShape(10, 10, "Rest of US");
+            enumVal4 = new TextShape(10, 10, "Outside US");
+        }
+        else {
+            title = new TextShape(10, 10, "Hobby Legend");
+            enumVal1 = new TextShape(10, 10, "Reading");
+            enumVal2 = new TextShape(10, 10, "Music");
+            enumVal3 = new TextShape(10, 10, "Sports");
+            enumVal4 = new TextShape(10, 10, "Art");
+        }
+
+        window.addShape(title);
+        window.addShape(enumVal1);
+        window.addShape(enumVal2);
+        window.addShape(enumVal3);
+        window.addShape(enumVal4);
+    }
+
+
+    /**
+     * Updates all glyphs in response to signals send to GUI.
+     */
+    private void updateGlyphs() {
+        window.removeAllShapes();
+        int i = 0;
+        Song song;
+
+        for (int x = 0; x < 3; x++) {
+            for (int y = 0; y < 3; y++) {
+                song = list.get(pageNumber + i);
+                generateGlyph(x, y, song);
+                i++;
+
+                if (pageNumber + i >= list.size()) {
+                    return;
                 }
             }
-
-            if (pEarliest != -1) {
-                // The case when lh String is already where it should be.
-                swapElements(lh, pEarliest);
-            }
         }
     }
 
 
     /**
-     * Determines if second String comes earlier than the first.
+     * Generates a glyph on the Window representing one Song in a 3 x 3 mesh.
      * 
-     * @param str1
-     * @param str2
-     * @return Whether str2 is earlier than str1.
+     * @param x
+     *            The row index of the glyph.
+     * @param y
+     *            The column index of the glyph.
+     * @param song
+     *            A Song from the list.
      */
-    public boolean foundNewEarliest(String str1, String str2) {
-        String[] str = new String[] { str1, str2 };
-        String[] sorted = new String[] { str1, str2 };
-        Arrays.sort(sorted);
-        if (str[0] != sorted[0]) {
-            return true;
+    private void generateGlyph(int x, int y, Song song) {
+        // generate column
+        int xPos = (int)((x + 1) * .25 * window.getGraphPanelWidth()
+            - columnWidth / 2);
+        int yPos = (int)((y + 1) * .25 * window.getGraphPanelHeight());
+        window.addShape(new Shape(xPos, yPos, columnWidth, columnHeight));
+
+        int xLike = xPos + columnWidth;
+
+        // generate bars
+        int[][] info = song.getInfo(represent);
+
+        int xMagentaH = barLength(info[0][0]);
+        int xBlueH = barLength(info[1][0]);
+        int xOrangeH = barLength(info[2][0]);
+        int xGreenH = barLength(info[3][0]);
+
+        // heards bars
+        Shape magentaH = new Shape(xPos - xMagentaH, yPos + 3 * barWidth,
+            xMagentaH, barWidth, Color.MAGENTA);
+        Shape blueH = new Shape(xPos - xBlueH, yPos + 2 * barWidth, xBlueH,
+            barWidth, Color.BLUE);
+        Shape orangeH = new Shape(xPos - xOrangeH, yPos + barWidth, xOrangeH,
+            barWidth, Color.ORANGE);
+        Shape greenH = new Shape(xPos - xGreenH, yPos, xGreenH, barWidth,
+            Color.GREEN);
+
+        // likes bars
+        Shape magentaL = new Shape(xLike, yPos + 3 * barWidth, barLength(
+            info[0][1]), barWidth, Color.MAGENTA);
+        Shape blueL = new Shape(xLike, yPos + 2 * barWidth, barLength(
+            info[1][1]), barWidth, Color.BLUE);
+        Shape orangeL = new Shape(xLike, yPos + barWidth, barLength(info[2][1]),
+            barWidth, Color.ORANGE);
+        Shape greenL = new Shape(xLike, yPos, barLength(info[3][1]), barWidth,
+            Color.GREEN);
+
+        // add TextShape description
+        String description = song.getTitle();
+        if (represent != "title") {
+            description += " " + song.getTitle();
         }
 
-        return false;
+        window.addShape(new TextShape(xPos, yPos - columnHeight - 5, song
+            .getTitle()));
+        window.addShape(magentaH);
+        window.addShape(blueH);
+        window.addShape(orangeH);
+        window.addShape(greenH);
+
+        window.addShape(magentaL);
+        window.addShape(blueL);
+        window.addShape(orangeL);
+        window.addShape(greenL);
     }
 
 
     /**
-     * Swaps the p1 and p2 entries in the param array and subsequently in the
-     * list.
+     * Returns the length of a bar based on count of likes or heards.
      * 
-     * @param p1
-     *            Index of first Song.
-     * @param p2
-     *            Index of second Song.
+     * @param count
+     *            Number of likes or heards.
+     * @return Length of a bar.
      */
-    public void swapElements(int p1, int p2) {
-        if (p2 != p1 && p2 <= size && p1 <= size) {
-            String temp = param[p1];
-            param[p1] = param[p2];
-            param[p2] = temp;
-
-            Node node1 = getNodeAt(p1);
-            Node node2 = getNodeAt(p2);
-
-            Song sTemp = node1.data;
-            node1.data = node2.data;
-            node2.data = sTemp;
-        }
-    }
-
-
-    private Node getNodeAt(int index) {
-        if (index < 0 || size() <= index) {
-            throw new IndexOutOfBoundsException("No element exists at "
-                + index);
-        }
-
-        Node curr = head;
-
-        for (int i = 0; i < index; i++) {
-            curr = curr.next;
-        }
-
-        return curr;
-    }
-
-
-    public LListIterator iterator() {
-        return new LListIterator();
-    }
-
-
-    private class LListIterator implements Iterator<Song> {
-
-        private Node curr;
-
-
-        public LListIterator() {
-            curr = head;
-        }
-
-
-        public boolean hasNext() {
-            return curr.next != null;
-        }
-
-
-        @Override
-        public Song next() {
-            curr = curr.next();
-            curr = curr.next;
-            return curr.getData();
-        }
+    private int barLength(int count) {
+        return barLengthFactor * count;
     }
 }
